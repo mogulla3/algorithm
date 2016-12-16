316f0b5114a1e26106c02d7418a071d630300222
mogulla3 <hs.iarn3@gmail.com>
2016-12-16 22:52:14 +0900

    Add linear search program written by ruby

---
+++ linear_search.rb
@@ -0,0 +1,27 @@
+# # 線形探索スクリプト
+#
+# ## 仕様
+# - 入力値は数値のカンマ区切り. パイプでスクリプトに渡す
+# - 数値以外の入力値は想定していない
+# - 探したい値を第1引数に渡す
+#
+# ## 例: '1,2,3'という入力値から'2'を探す
+# $ echo '1,2,3' | ruby liner_search.rb 2
+# Found '2'
+# Number of searches: 2
+input = $stdin.gets.chomp.split(',')
+target = ARGV[0].to_i
+num_search = 0
+
+input.each do |value|
+  num_search += 1
+  if value.to_i == target
+    puts "Found '#{target}'"
+    puts "Number of searches: #{num_search}"
+    exit 0
+  end
+end
+
+puts 'Not Found..'
+puts "Number of searches: #{num_search}"
+exit 1
