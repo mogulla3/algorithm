e4d974c32be7868b11daf1baf8797c22775e9a8a
mogulla3 <hs.iarn3@gmail.com>
2018-03-21 14:50:17 +0900

    Refactor avg.rb

--- ruby/avg.rb
+++ ruby/avg.rb
@@ -1,19 +1,16 @@
+values = gets.split(" ").map(&:to_i)
 
-array = [1,2,3,5,6,7,12,124,54,43,89]
-count = 0 
-sum = 0 
-avg = 0.0 
-i = 0
-
-while array[i] do
-  sum += array[i]
+count = 0
+sum = 0
+values.each do |value|
+  sum += value
   count += 1
-  i += 1
 end
 
-if array.length == 0
-  puts "avg = 0.0"
-else
-  avg = sum / count
-  puts "avg = #{avg}"
-end
+avg = if count.zero?
+        0.0
+      else
+        sum.to_f / count.to_f
+      end
+
+puts "average: #{avg}"
